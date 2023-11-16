d5e8f016fd6de3076b9cbff2a86c3a8d85d6539e
Devika Mujgule <mujgule.d@northeastern.edu>
2023-11-16 14:18:32 -0500

    feat: moving website to react app

---
+++ protfolio/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+// import styled from 'styled-components';
+
+// const StyledDiv = styled.div`
+// background: #3399ff;
+// `
+
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <h1>Hey, I am Devika Mujgule</h1>
+        <ul>
+          <li>I'm a frontend dev, plant mom, anime lover and gym go-er</li>
+          <li>Please reach out to me at mujgule.d@northeastern.edu</li>
+
+        </ul>
+        <h3>My <a href="https://www.linkedin.com/in/devikamujgule/">LinkedIn</a></h3>
+        <h3>My <a href="https://github.com/mujgule-devika/">Github</a></h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
